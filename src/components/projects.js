92d9d26e91f95d042f9b827fde82a9e8a2a91e1d
Riley Nielson <rileyanielson@gmail.com>
2023-08-30 09:17:51 -0600

    updating to add projects

---
+++ src/components/projects.js
@@ -0,0 +1,59 @@
+const projects = [
+    {
+      key: 1,
+      src: "https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg",
+      alt: "HTML5 Powered",
+    },
+    {
+      key: 2,
+      src: "https://www.vectorlogo.zone/logos/nodejs/nodejs-horizontal.svg",
+      alt: "Nodejs logo and wordmark",
+    },
+    {
+      key: 3,
+      src: "https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg",
+      alt: "React logo and wordmark",
+    },
+    {
+      key: 4,
+      src: "https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg",
+      alt: "CSS3 logo and wordmark",
+    },
+    {
+      key: 5,
+      src: "https://www.vectorlogo.zone/logos/mongodb/mongodb-ar21.svg",
+      alt: "MongoDB logo and wordmark",
+    },
+    {
+      key: 6,
+      src: "./Images/blankImage.png",
+      alt: "",
+    },
+    {
+      key: 7,
+      src: "https://www.vectorlogo.zone/logos/git-scm/git-scm-ar21.svg",
+      alt: "Git logo and wordmark",
+    },
+    {
+      key: 8,
+      src: "https://www.vectorlogo.zone/logos/jquery/jquery-horizontal.svg",
+      alt: "Jquery logo and wordmark",
+    },
+    {
+      key: 9,
+      src: "./Images/blankImage.png",
+      alt: "",
+    },
+    {
+      key: 10,
+      src: "./Images/blankImage.png",
+      alt: "",
+    },
+    {
+      key: 11,
+      src: "https://www.vectorlogo.zone/logos/javascript/javascript-horizontal.svg",
+      alt: "Javascript logo and wordmark",
+    },
+  ];
+  
+  export default projects;
